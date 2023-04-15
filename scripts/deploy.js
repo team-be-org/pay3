95a2c530ee135060df7a7b012bf3a953d707a32a
haruxx <harukatarotaro@gmail.com>
2023-04-15 09:02:38 +0900

    deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,56 +1,20 @@
-// This is a script for deploying your contracts. You can adapt it to deploy
-// yours, or create new ones.
-
-const path = require("path");
+const hre = require("hardhat");
+const fs = require("fs");
+const { artifacts } = require("hardhat");
 
 async function main() {
-  // This is just a convenience check
-  if (network.name === "hardhat") {
-    console.warn(
-      "You are trying to deploy a contract to the Hardhat Network, which" +
-        "gets automatically created and destroyed every time. Use the Hardhat" +
-        " option '--network localhost'"
-    );
-  }
 
-  // ethers is available in the global scope
-  const [deployer] = await ethers.getSigners();
-  console.log(
-    "Deploying the contracts with the account:",
-    await deployer.getAddress()
-  );
-
-  console.log("Account balance:", (await deployer.getBalance()).toString());
-
-  const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy();
+  const BadgeToken = await hre.ethers.getContractFactory("Pay3");
+  console.log('Deploying Contract...');
+  const token = await BadgeToken.deploy();
   await token.deployed();
 
-  console.log("Token address:", token.address);
-
-  // We also save the contract's artifacts and address in the frontend directory
-  saveFrontendFiles(token);
-}
-
-function saveFrontendFiles(token) {
-  const fs = require("fs");
-  const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
-
-  if (!fs.existsSync(contractsDir)) {
-    fs.mkdirSync(contractsDir);
-  }
-
   fs.writeFileSync(
-    path.join(contractsDir, "contract-address.json"),
-    JSON.stringify({ Token: token.address }, undefined, 2)
-  );
-
-  const TokenArtifact = artifacts.readArtifactSync("Token");
-
-  fs.writeFileSync(
-    path.join(contractsDir, "Token.json"),
-    JSON.stringify(TokenArtifact, null, 2)
-  );
+    "frontend2/src/utils/contractAddress.json",
+    '{"contractAddress": "' + token.address + '"}'
+  )
+  fs.copyFileSync("artifacts/contracts/Pay3.sol/Pay3.json", "frontend2/src/utils/Pay3.json")
+  console.log("Pay3 deployed to:", token.address);
 }
 
 main()
