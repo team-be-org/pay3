51f68f95647423488f683960e77b6577cdee91a7
haruxx <harukatarotaro@gmail.com>
2023-04-15 16:43:22 +0900

    fixed withdraw function call

--- frontend2/src/App.js
+++ frontend2/src/App.js
@@ -1,21 +1,22 @@
 import { useState, useEffect } from 'react'
 import { ethers } from 'ethers'
-import Modal from './components/Modal'
-import openseaLogo from './assets/opensea-logo.png'
 import myNft from './utils/Pay3.json'
 import './App.css';
 
-const OPENSEA_LINK = 'https://opensea.io/collection/pay3';
 const CONTRACT_ADDRESS = require("./utils/contractAddress.json").contractAddress;
-const ethScanContractURL = 'https://etherscan.io/address/' + CONTRACT_ADDRESS + '#writeContract';
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("")
-  const [miningAnimation, setMiningAnimation] = useState(false)
   const [display, setDisplay] = useState("...")
   const [tokenId, setTokenId] = useState(1)
   const [ethValue, setEthValue] = useState("")
+  const [balance, setBalance] = useState(0)
 
+  // check if wallet is connected
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
 
@@ -33,8 +34,6 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account)
-      //setupEventListener()
-      // getNumberOfMinted()
     } else {
       console.log("No authorized account found")
     }
@@ -50,16 +49,30 @@ const App = () => {
       }
 
       const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-      
       console.log("Connected", accounts[0])
       setCurrentAccount(accounts[0])
-
-      //setupEventListener()
     } catch (error) {
       console.log(error)
     }
   }
 
+  // get Balance for display continually
+  async function getBalance() {
+    const { ethereum } = window;
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const balance = await provider.getBalance(currentAccount);
+      if (balance) {
+        setBalance(ethers.utils.formatEther(balance));
+        console.log("ETH balance", ethers.utils.formatEther(balance));
+      }
+      console.log("ETH balance", ethers.utils.formatEther(balance));
+    } else {
+      console.log("Ethereum object doesn't exist");
+    }
+  }
+
+  // mint NFT as virtual wallet
   async function mintNFT() {
     try {
       const { ethereum } = window;
@@ -72,6 +85,7 @@ const App = () => {
         setDisplay("start minting...")
         let response = await connectedContract.mint(options)
         setDisplay("mint finished")
+        getBalance()
         console.log(`tokenId: ${response}`);
       } else {
         console.log("Ethereum object doesn't exist");
@@ -98,6 +112,26 @@ const App = () => {
         const options = {value: ethers.utils.parseEther(ethValue)}
         await connectedContract.usersendETH(tokenId, options)
         setDisplay("charge finished")
+        getBalance()
+      } else {
+        console.log("Ethereum object doesn't exist");
+      }
+    } catch (error) {
+      console.log(error);
+      setDisplay("error");
+    }
+  }
+
+  const WithdrawExecution = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner()
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myNft.abi, signer)
+        await connectedContract.userWithdraw(tokenId)
+        setDisplay("charge finished")
+        getBalance()
       } else {
         console.log("Ethereum object doesn't exist");
       }
@@ -133,6 +167,7 @@ const App = () => {
 
   useEffect(() => {
     checkIfWalletIsConnected()
+    getBalance()
   })
 
   const handleChange = (e) => {
@@ -159,39 +194,39 @@ const App = () => {
     </div>
   )
 
+  // connected wallet UI
   const renderWalletControlUI = () => (
-    <div style={{display: 'flex', justifyContent: 'center'}}>
+    <div>
       <div style={{marginRight: '10px'}}>
+      <span style={{ color: "white", fontSize: "20px" }}> Charge Value: </span>
         <input className="text-box" placeholder="val (ETH)" value={ethValue} onChange={handleEthValueChange} type="text" style={{width: "100px"}} />
       </div>
-      <div>
-        <button onClick={ChargeExecution} className="cta-button mint-button">
+      <p/>
+      <div style={{display: 'flex', justifyContent: 'center'}}></div>
+        <button onClick={ChargeExecution} className="cta-button mint-button" style={{width: "150px"}}>
           Charge
         </button>
-      </div>
-    </div>
-  )
-
-  const renderSubscriptionOnOffUI = () => (
-    <div style={{display: 'flex', justifyContent: 'center'}}>
+        <button onClick={WithdrawExecution} className="cta-button mint-button" style={{width: "150px"}}>
+          Withdraw
+        </button>
+      <div/>
+      <span style={{ color: "white", fontSize: "20px", marginTop: "30px" }}> Subscription switch</span>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
       <div>
-        <button onClick={SubscriptionOn} className="cta-button mint-button">
+        <button onClick={SubscriptionOn} className="cta-button mint-button" style={{width: "150px"}}>
           On
         </button>
-        <button onClick={SubscriptionOff} className="cta-button mint-button">
+        <button onClick={SubscriptionOff} className="cta-button mint-button"  style={{width: "150px"}}>
           Off
         </button>
       </div>
     </div>
+    </div>
   )
+
 
   return (
     <div className="App">
-      {
-        miningAnimation ? (
-          <Modal />
-        ) : null
-      }
       <div className="container">
         <div className="header-container">
           <p className="header gradient-text">Pay3</p>
@@ -200,40 +235,23 @@ const App = () => {
           {currentAccount === "" ? renderNotConnectedContainer() :renderMintUI()}
           <hr/>
           <div style={{marginRight: '10px'}}>
-          wallet num:
+          <span style={{ color: "white", fontSize: "20px" }}> Wallet Number: </span>
           <input className="text-box" placeholder="Wallet Num" value={tokenId} onChange={handleChange} type="text" style={{width: "150px"}}/>
           <p/>
         </div>
           {currentAccount === "" ? null :renderWalletControlUI()}
           <p/>
-          {currentAccount === "" ? null :renderSubscriptionOnOffUI()}
-          <p/>
-          {/* <p className="sub-text">
-            {mintTotal} of 100 NFTs minted.
-          </p> */}
+          <span style={{ color: "white", fontSize: "20px" }}> 
+          current Balance: {balance}
+          </span>
           <textarea
-            style={{ height: "200px", width: "80%"}}
+            style={{ height: "200px", width: "80%", fontSize: "18px"}}
             className="text-area"
             value={display}
           />
           <p/>
-          <a
-            className="opensea-button"
-            href={OPENSEA_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={openseaLogo} alt="opensea-logo" className="opensea-logo" />View on OpenSea</a>
-          <p className="explain-text">
-          <br/><a href={ethScanContractURL} target="_blank" rel="noreferrer">直コンはここ</a>
-          </p>
         </div>
         <div className="footer-container">
-          {/* {currentAccount !== "" ?
-            (<text className='footer-text' onClick={DisconnectWallet}>disconnect wallet</text>)
-            :
-            ("...")
-          } */}
         </div>
       </div>
     </div>
